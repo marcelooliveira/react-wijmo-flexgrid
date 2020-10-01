58e6204694e0c6d982992fda79080b36fc193082
marcelooliveira <mclricardo@gmail.com>
2020-10-01 01:13:53 -0300

    Import feature

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,24 +1,53 @@
 ﻿import '@grapecity/wijmo.styles/wijmo.css';
-import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import '@grapecity/wijmo.react.grid';
+import React, { useState } from 'react';
 import { FlexGrid, FlexGridColumn } from '@grapecity/wijmo.react.grid';
+import * as wjcGridXlsx from "@grapecity/wijmo.grid.xlsx";
 import { recentSales } from "../data/data";
 
 export const Dashboard = () => {
 
-  const[sales, setSales] = new useState(recentSales);
+  const [sales, setSales] = new useState(recentSales);
+  const [flexGrid, setFlexGrid] = useState({});
+  const [includeColumnHeaders, setIncludeColumnHeaders] = useState(true);
+
+  var initializeFlexGrid = (flexGrid) => {
+      setFlexGrid(flexGrid);
+  }
+
+  var load = () => {
+    let fileInput = document.getElementById("importFile");
+    if (fileInput.files[0]) {
+        wjcGridXlsx.FlexGridXlsxConverter.loadAsync(flexGrid, fileInput.files[0], { includeColumnHeaders: includeColumnHeaders });
+    }
+  }
 
   return (<div className="container-fluid">
-
-  <FlexGrid itemsSource={sales}>
-    <FlexGridColumn width={50} binding='id' header="ID" />
-    <FlexGridColumn width={200} binding='client' header="Client" />
-    <FlexGridColumn width={320} binding='description' header="Description" />
-    <FlexGridColumn width={100} binding='value' header="Value"/>
-    <FlexGridColumn width={100} binding='itemCount' header="Quantity" />
-    <FlexGridColumn width={100} binding='soldBy' header="Sold By" />
-    <FlexGridColumn width={100} binding='country' header="Country" />
-  </FlexGrid>
-</div>);
+            <div className="row">
+              <FlexGrid itemsSource={sales} initialized={initializeFlexGrid}>
+                <FlexGridColumn width={50} binding='id' header="ID" />
+                <FlexGridColumn width={200} binding='client' header="Client" />
+                <FlexGridColumn width={320} binding='description' header="Description" />
+                <FlexGridColumn width={100} binding='value' header="Value"/>
+                <FlexGridColumn width={100} binding='itemCount' header="Quantity" />
+                <FlexGridColumn width={100} binding='soldBy' header="Sold By" />
+                <FlexGridColumn width={100} binding='country' header="Country" />
+              </FlexGrid>
+            </div>
+            <div className="row">
+                <div className="col-md-6 col-xs-12">
+                    <div className="form-inline well well-lg">
+                        <input type="file" className="form-control" style={{ width: '250px' }} id="importFile" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel.sheet.macroEnabled.12"/>
+                        <input type="button" onClick={load} className="btn btn-default" value="Import"/>
+                        <div className="checkbox">
+                            <label>
+                                <input value={includeColumnHeaders} type="checkbox"/> Include Column Header
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>);
 }
 
