ba7f222c4efcbf2b79d7782fc6240247e2595527
marcelooliveira <mclricardo@gmail.com>
2020-09-30 23:27:24 -0300

    Simple data table
    Simple data table with Wijmo Flexgrid

---
+++ src/components/Dashboard.js
@@ -0,0 +1,24 @@
+﻿import '@grapecity/wijmo.styles/wijmo.css';
+import React, { useState } from 'react';
+import '@grapecity/wijmo.react.grid';
+import { FlexGrid, FlexGridColumn } from '@grapecity/wijmo.react.grid';
+import { recentSales } from "../data/data";
+
+export const Dashboard = () => {
+
+  const[sales, setSales] = new useState(recentSales);
+
+  return (<div className="container-fluid">
+
+  <FlexGrid itemsSource={sales}>
+    <FlexGridColumn width={50} binding='id' header="ID" />
+    <FlexGridColumn width={200} binding='client' header="Client" />
+    <FlexGridColumn width={320} binding='description' header="Description" />
+    <FlexGridColumn width={100} binding='value' header="Value"/>
+    <FlexGridColumn width={100} binding='itemCount' header="Quantity" />
+    <FlexGridColumn width={100} binding='soldBy' header="Sold By" />
+    <FlexGridColumn width={100} binding='country' header="Country" />
+  </FlexGrid>
+</div>);
+}
+
